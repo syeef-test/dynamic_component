5d9c29794d77d39e69b002f0dcfa14668ac6a8f1
syeef <syeefislam@yahoo.com>
2024-03-29 22:28:12 +0530

    add text done

--- src/components/ChildComponents.jsx
+++ src/components/ChildComponents.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-function ChildComponents() {
-  return <div>ChildComponents</div>;
+function ChildComponents({ type }) {
+  const displayData = () => {
+    switch (type) {
+      case "text":
+        return <input type="text" />;
+      default:
+        return null;
+    }
+  };
+  return <div>{displayData()}</div>;
 }
 
 export default ChildComponents;
--- src/components/DynamicComponents.jsx
+++ src/components/DynamicComponents.jsx
@@ -5,13 +5,16 @@ function DynamicComponents() {
   const [comp, setComp] = useState([]);
 
   const addComponent = (comp_type) => {
-    setComp((prev) => [...prev, { id: Math.random, type }]);
+    setComp((prev) => [...prev, { id: Math.random, comp_type }]);
   };
 
   return (
     <>
       <button onClick={() => addComponent("text")}>Add Text</button>
-      <ChildComponents />
+
+      {comp.map((item) => (
+        <ChildComponents key={item.id} type={item.comp_type} />
+      ))}
     </>
   );
 }
